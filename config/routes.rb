2c6cbee79c367265cc4eabc24fa097e456fe986c
water23 <aki.air@icloud.com>
2016-01-23 13:36:14 +0000

    login logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
   get 'signup', to: 'users#new'
+  get 'login', to: 'session#new'
+  post 'login', to: 'session#create'
+  delete 'logout', to: 'session#destroy'
   
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 end
