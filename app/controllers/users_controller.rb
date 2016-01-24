e8dce25c02c4a49a2ecc628a78277d56c44534e0
water23 <aki.air@icloud.com>
2016-01-24 07:57:55 +0000

    add new column

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,6 +37,6 @@ class UsersController < ApplicationController
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :introduction, :birthday)
   end
 end
